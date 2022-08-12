cf4a96cb89fadd7ca2ed36a914a459c93768d613
sungjin <sungjin0757@naver.com>
2022-08-12 16:55:10 +0900

    first commit

---
+++ src/main/java/basic/fileloadapi/FileLoadApiApplication.java
@@ -0,0 +1,13 @@
+package basic.fileloadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileLoadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileLoadApiApplication.class, args);
+	}
+
+}
