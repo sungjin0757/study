f74a8a148c94e07686f48e038bf34df7a1a181c0
sungjin <sungjin0757@naver.com>
2022-08-18 00:57:56 +0900

    master: Modify application.yml, RedisConfiguration

--- src/main/java/basic/redisstudy/common/config/RedisConfig.java
+++ src/main/java/basic/redisstudy/common/config/RedisConfig.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -29,7 +30,7 @@ public class RedisConfig {
     }
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
+    public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisProperty.getHost(), redisProperty.getPort());
     }
 
