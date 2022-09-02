61d2360d90f7632401bc8ccbbae7292d30c9c577
sungjin <sungjin0757@naver.com>
2022-09-02 20:05:19 +0900

    First Commit

---
+++ chunk-oriented-tasklet/src/main/java/basic/chunkorientedtasklet/ChunkOrientedTaskletApplication.java
@@ -0,0 +1,13 @@
+package basic.chunkorientedtasklet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunkOrientedTaskletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunkOrientedTaskletApplication.class, args);
+	}
+
+}
