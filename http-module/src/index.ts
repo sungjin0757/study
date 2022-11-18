723874925e4ed65d230cc845e4539b89715400db
sungjin <sungjin0757@naver.com>
2022-11-18 17:03:25 +0900

    Add index.ts

---
+++ http-module/src/index.ts
@@ -0,0 +1,7 @@
+import { server1 } from "./http-module/server1";
+import { server1_1 } from './http-module/server1-1';
+import { restServer } from './rest/restServer';
+
+// server1();
+// server1_1();
+restServer();
