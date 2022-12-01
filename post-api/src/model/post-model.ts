fc058e967f3a09295f37cb6a3dbe7b4c57155f8d
sungjin <sungjin0757@naver.com>
2022-12-01 11:27:59 +0900

    Add PostModel

---
+++ post-api/src/model/post-model.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+import { IUser } from './user-model';
+
+export interface IPost {
+    _id?: mongoose.Types.ObjectId;
+    title: string;
+    content: string;
+    user: IUser;
+}
+
+const postSchema: Schema<IPost> = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        required: true
+    }
+}, {timestamps: true});
+
+export const PostModel = mongoose.model<IPost>('Post', postSchema);
