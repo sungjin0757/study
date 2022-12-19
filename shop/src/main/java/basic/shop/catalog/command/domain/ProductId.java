91aa463f9b7c342febc770b853630b1a83e3184c
sungjin <sungjin0757@naver.com>
2022-12-19 22:44:31 +0900

    Add ProductId value in catalog aggregate

--- shop/src/main/java/basic/shop/catalog/command/domain/ProductId.java
+++ shop/src/main/java/basic/shop/catalog/command/domain/ProductId.java
@@ -7,13 +7,14 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.io.Serializable;
 import java.util.Objects;
 
 @Embeddable
 @Access(AccessType.FIELD)
 @Getter
 @AllArgsConstructor
-public class ProductId {
+public class ProductId implements Serializable {
     @Column(name = "product_id")
     private String id;
 
