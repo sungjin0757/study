ac352ee444a0eacdc16070ca297ff100284b8b99
sungjin <sungjin0757@naver.com>
2022-12-19 21:39:55 +0900

    Add ProductId value

---
+++ shop/src/main/java/basic/shop/catalog/command/domain/ProductId.java
@@ -0,0 +1,33 @@
+package basic.shop.catalog.command.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+@Access(AccessType.FIELD)
+@Getter
+@AllArgsConstructor
+public class ProductId {
+    @Column(name = "product_id")
+    private String id;
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == this)
+            return true;
+        if(!(o instanceof ProductId productId))
+            return false;
+        return Objects.equals(id, productId.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id.hashCode();
+    }
+}
