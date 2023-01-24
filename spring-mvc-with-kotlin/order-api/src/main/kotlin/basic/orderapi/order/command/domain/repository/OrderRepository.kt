2b46bd30a0e6a56e0d1c9b0f8178348a355046e8
sungjin <sungjin0757@naver.com>
2023-01-24 22:25:34 +0900

    Modify OrderRepository

--- spring-mvc-with-kotlin/order-api/src/main/kotlin/basic/orderapi/order/command/domain/repository/OrderRepository.kt
+++ spring-mvc-with-kotlin/order-api/src/main/kotlin/basic/orderapi/order/command/domain/repository/OrderRepository.kt
@@ -4,7 +4,15 @@ import basic.orderapi.order.command.domain.entity.Order
 import basic.orderapi.order.command.domain.value.OrderNo
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
 import org.springframework.data.repository.CrudRepository
+import java.util.*
+import java.util.concurrent.ThreadLocalRandom
 
 interface OrderRepository: CrudRepository<Order, OrderNo>,
     OrderRepositoryCustom, QuerydslPredicateExecutor<Order> {
+
+        fun  nextOrderNo(): OrderNo {
+            var randomNo = ThreadLocalRandom.current().nextInt(900000) + 100000;
+            var number = String.format("%tY%<tm%<td%<tH-%d", Date(), randomNo)
+            return OrderNo(number)
+        }
 }
