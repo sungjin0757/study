3a30552cc3b4ab6aaa86e15841691ba45b6c924d
sungjin <sungjin0757@naver.com>
2023-03-02 16:35:59 +0900

    First Commit

---
+++ spring-async-events/src/main/kotlin/basic/asyncevents/AsyncEventsApplication.kt
@@ -0,0 +1,11 @@
+package basic.asyncevents
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsyncEventsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AsyncEventsApplication>(*args)
+}
---
+++ spring-async-events/src/test/kotlin/basic/asyncevents/AsyncEventsApplicationTests.kt
@@ -0,0 +1,13 @@
+package basic.asyncevents
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsyncEventsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
