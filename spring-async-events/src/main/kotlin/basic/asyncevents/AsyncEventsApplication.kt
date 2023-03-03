d54cde2d73d7e1be7db8cf4896a6d94d95bc1d2a
sungjin <sungjin0757@naver.com>
2023-03-03 21:59:59 +0900

    Add First Setting

--- spring-async-events/src/main/kotlin/basic/asyncevents/AsyncEventsApplication.kt
+++ spring-async-events/src/main/kotlin/basic/asyncevents/AsyncEventsApplication.kt
@@ -2,7 +2,11 @@ package basic.asyncevents
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
+@EnableJpaAuditing
 @SpringBootApplication
 class AsyncEventsApplication
 
